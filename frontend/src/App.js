ed25886cfa048e3e3ff908655d22d122d7a132d1
Ryan Chalmers <ryan@spotwork.co>
2024-11-09 10:17:41 -0500

    react refresher section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import GoalList from './components/GoalList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="course-goals">
+    <h2>Course Goals</h2>
+    <GoalList/>
+  </div>
 }
 
 export default App;
