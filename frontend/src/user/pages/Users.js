28a911fb486e1e5349950f1f757cf8b714f35d1f
Ryan Chalmers <ryan@spotwork.co>
2024-11-09 13:28:04 -0500

    added user card list and card item

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -3,7 +3,15 @@ import React from "react";
 import UsersList from '../components/UsersList'
 
 const Users = () => {
-    return <UsersList/>;
+    const USERS = [
+        {
+            id: 'u1',
+            name: 'Ryan Chalmers',
+            image: 'https://picsum.photos/id/237/200/300',
+            places: 3
+        }
+    ];
+    return <UsersList items={USERS}/>;
 };
 
 export default Users;
