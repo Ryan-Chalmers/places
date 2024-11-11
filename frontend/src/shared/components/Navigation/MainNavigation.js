75ce8fc7664f718de13e65b1860df3b06212a351
Ryan Chalmers <ryan@spotwork.co>
2024-11-11 11:21:24 -0500

    working on navigtion

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -1,23 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
-import './MainNavigation.css'
+import "./MainNavigation.css";
 import MainHeader from "./MainHeader";
+import SideDrawer from "./SideDrawer";
+import NavLinks from "./NavLinks";
 
-const MainNavigation = props => {
-    return <MainHeader>
-        <button className="main-navigation__menu-btn">
-            <span/>
-            <span/>
-            <span/>
-        </button>
-        <h1 className='main-navigation__title'>
-            <Link to='/'>YourPlaces</Link>
-        </h1>
-        <nav>
-            ...
+const MainNavigation = (props) => {
+  return (
+    <>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks/>
         </nav>
-    </MainHeader>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">YourPlaces</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </>
+  );
 };
 
 export default MainNavigation;
