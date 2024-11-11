e87f9d1c170373a9cb0459a2429355eec23c00f0
Ryan Chalmers <ryan@spotwork.co>
2024-11-11 12:49:26 -0500

    added routing with route params

---
+++ frontend/src/places/pages/UserPlaces.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous skyscrapers in the world",
+    imageURL:
+      "https://fastly.picsum.photos/id/870/200/300.jpg?blur=2&grayscale&hmac=ujRymp644uYVjdKJM7kyLDSsrqNSMVRPnGU99cKl6Vs",
+    address: "20 W 34th St., New York, NY 10001, United States",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9882393,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous skyscrapers in the world",
+    imageURL:
+      "https://fastly.picsum.photos/id/870/200/300.jpg?blur=2&grayscale&hmac=ujRymp644uYVjdKJM7kyLDSsrqNSMVRPnGU99cKl6Vs",
+    address: "20 W 34th St., New York, NY 10001, United States",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9882393,
+    },
+    creator: "u2",
+  },
+];
+
+const UserPlaces = () => {
+  const userId = useParams().userId;
+  console.log(userId);
+  const loadedPlaces = DUMMY_PLACES.filter(places => places.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
+};
+
+export default UserPlaces;
