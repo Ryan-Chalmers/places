b5fd196e54a32fc071a08d5e113ac6e33a7905af
Ryan Chalmers <ryan@spotwork.co>
2024-11-11 14:13:25 -0500

    added somewhat janky google maps integration

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -4,6 +4,7 @@ import "./PlaceItem.css";
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
 import Modal from "../../shared/components/UIElements/Modal";
+import Map from "../../shared/components/UIElements/Map"
 
 const PlaceItem = (props) => {
   const [showMap, setShowMap] = useState(false);
@@ -21,9 +22,7 @@ const PlaceItem = (props) => {
         footerClass="place-item__modal-actions"
         footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
       >
-        <div className="map-container">
-          <h2>THE MAP!</h2>
-        </div>
+        <Map center={props.coordinates} zoom={16}/>
       </Modal>
       <li className="place-item">
         <Card className="place-itme__content">
---
+++ frontend/src/shared/components/UIElements/Map.js
@@ -0,0 +1,29 @@
+import React, { useRef, useEffect } from "react";
+
+import "./Map.css";
+
+const Map = (props) => {
+  const mapRef = useRef();
+
+  const { center, zoom } = props;
+
+  useEffect(() => {
+    const map = new window.google.maps.Map(mapRef.current, {
+      center: center,
+      zoom: zoom,
+      mapId: "DEMO_MAP_ID"
+    });
+
+    new window.google.maps.marker.AdvancedMarkerElement({ position: center, map: map, title: "SAMPLE" });
+  }, [center, zoom]);
+
+  return (
+    <div
+      ref={mapRef}
+      className={`map ${props.className}`}
+      style={props.style}
+    ></div>
+  );
+};
+
+export default Map;
