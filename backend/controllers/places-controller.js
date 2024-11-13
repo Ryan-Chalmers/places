28984c59fa4cc39ee562f052bab2e716b3fcc623
Ryan Chalmers <ryan@spotwork.co>
2024-11-13 14:31:31 -0500

    cleaned up routes file, moved logic to controllers

---
+++ backend/controllers/places-controller.js
@@ -0,0 +1,48 @@
+const HttpError = require("../models/http-error");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous skyscrapers in the world",
+    imageURL:
+      "https://fastly.picsum.photos/id/870/200/300.jpg?blur=2&grayscale&hmac=ujRymp644uYVjdKJM7kyLDSsrqNSMVRPnGU99cKl6Vs",
+    address: "20 W 34th St., New York, NY 10001, United States",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9882393,
+    },
+    creator: "u1",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+
+  const place = DUMMY_PLACES.find((p) => p.id === placeId);
+
+  if (!place) {
+    return next(
+      new HttpError("Could not find a place for the provided id.", 404)
+    );
+  }
+
+  res.json({ place });
+}
+
+const getPlacesByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+
+  const userPlaces = DUMMY_PLACES.filter((p) => p.creator === userId);
+
+  if (!userPlaces) {
+    return next(
+      new HttpError("Could not find a place for the provided user id.", 404)
+    );
+  }
+
+  res.json({ userPlaces });
+}
+
+exports.getPlaceById = getPlaceById;
+exports.getPlacesByUserId = getPlacesByUserId;
--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,51 +1,11 @@
 const express = require("express");
 
-const HttpError = require("../models/http-error");
+const placesControllers =  require('../controllers/places-controller.js')
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous skyscrapers in the world",
-    imageURL:
-      "https://fastly.picsum.photos/id/870/200/300.jpg?blur=2&grayscale&hmac=ujRymp644uYVjdKJM7kyLDSsrqNSMVRPnGU99cKl6Vs",
-    address: "20 W 34th St., New York, NY 10001, United States",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9882393,
-    },
-    creator: "u1",
-  },
-];
+router.get("/:pid", placesControllers.getPlaceById);
 
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid;
-
-  const place = DUMMY_PLACES.find((p) => p.id === placeId);
-
-  if (!place) {
-    return next(
-      new HttpError("Could not find a place for the provided id.", 404)
-    );
-  }
-
-  res.json({ place });
-});
-
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-
-  const userPlaces = DUMMY_PLACES.filter((p) => p.creator === userId);
-
-  if (!userPlaces) {
-    return next(
-      new HttpError("Could not find a place for the provided user id.", 404)
-    );
-  }
-
-  res.json({ userPlaces });
-});
+router.get("/user/:uid", placesControllers.getPlacesByUserId);
 
 module.exports = router;
