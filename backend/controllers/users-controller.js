a5ce590822cba990d1e57c23c4601e926683c049
Ryan Chalmers <ryan.chalmers8@gmail.com>
2024-11-15 09:39:49 -0500

    added creat a user logic

--- backend/controllers/users-controller.js
+++ backend/controllers/users-controller.js
@@ -2,6 +2,7 @@ const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
+const User = require("../models/user");
 
 const DUMMY_USERS = [
   {
@@ -40,30 +41,44 @@ const login = (req, res, next) => {
   res.status(200).json({ message: "Logged in!" });
 };
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError("Invalid inputs passed, please check your data", 422);
+    return next(
+      new HttpError("Invalid inputs passed, please check your data", 422)
+    );
   }
 
-  const { email, password, name } = req.body;
+  const { email, password, name, image, places } = req.body;
+  let existingUser;
 
-  const hasUser = DUMMY_USERS.find((u) => u.email === email);
-
-  if (hasUser) {
-    return next(new HttpError("Email address already taken.", 422));
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (error) {
+    return next(new HttpError("Sign up failed, please try again later", 500));
   }
 
-  const newUser = {
-    id: uuidv4(),
+  if (existingUser) {
+    return next(
+      new HttpError("User exists already, please login instead", 422)
+    );
+  }
+
+  const newUser = new User({
     name,
     email,
     password,
-  };
+    image: "https://commons.wikimedia.org/wiki/File:Jard%C3%ADn_del_Pr%C3%ADncipe,_Mahan,_Ir%C3%A1n,_2016-09-22,_DD_21.jpg",
+    places,
+  });
 
-  DUMMY_USERS.push(newUser);
+  try {
+    await newUser.save();
+  } catch (error) {
+    return next(new HttpError("Sign up failed, please try again.", 500));
+  }
 
-  res.status(201).json({ user: newUser });
+  res.status(201).json({ user: newUser.toObject({ getters: true }) });
 };
 
 exports.getUsers = getUsers;
