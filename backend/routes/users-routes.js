a2088c89bb4b956a58c66acd407e97913ec36533
Ryan Chalmers <ryan.chalmers8@gmail.com>
2024-11-17 10:47:05 -0500

    configuring file upload middleware

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const { check } = require("express-validator");
 
 const usersController = require("../controllers/users-controller");
+const fileUpload = require("../middleware/file-upload")
 
 const router = express.Router();
 
 router.post(
   "/signup",
+  fileUpload.single('image'),
   [
     check("email").normalizeEmail().isEmail(),
     check("password").isLength({ min: 8 }),
