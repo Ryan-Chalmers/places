cfc16571601bab78d3ce1ab4c6b91e6e01db5347
Ryan Chalmers <ryan.chalmers8@gmail.com>
2024-11-17 11:30:28 -0500

    added file deletion

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 const { validationResult } = require("express-validator");
 const mongoose = require("mongoose");
 
@@ -121,6 +123,9 @@ const deletePlace = async (req, res, next) => {
   if (!place) {
     return next(new HttpError("Could not find place for this id.", 404));
   }
+
+  const imagePath = place.image;
+
   try {
     const sess = await mongoose.startSession();
     sess.startTransaction();
@@ -131,6 +136,10 @@ const deletePlace = async (req, res, next) => {
   } catch (error) {
     return next(new HttpError("Deleting place failed, please try again.", 500));
   }
+
+  fs.unlink(imagePath, (err) => {
+    console.log(err);
+  });
 
   res.status(200).json({ message: "Deleted place" });
 };
