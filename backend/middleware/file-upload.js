8de0fda63a2e71ba2083ce69260a8c4fbc6a1fc6
Ryan Chalmers <ryan.chalmers8@gmail.com>
2024-11-17 11:00:27 -0500

    added server file storage

--- backend/middleware/file-upload.js
+++ backend/middleware/file-upload.js
@@ -1,4 +1,5 @@
 const multer = require("multer");
+const { v4: uuidv4 } = require('uuid');
 
 const MIME_TYPE_MAP = {
   "image/png": "png",
@@ -10,11 +11,11 @@ const fileUpload = multer({
   limits: 500000,
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, "uploads/image");
+      cb(null, "uploads/images");
     },
     filename: (req, file, cb) => {
       const ext = MIME_TYPE_MAP[file.mimetype];
-      cb(null, uuid() + "." + ext);
+      cb(null, uuidv4() + "." + ext);
     },
   }),
   fileFilter: (req, file, cb) => {
